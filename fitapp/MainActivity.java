99eb534ccf3e1aca58e17923351138388d2c3c4e
CarlosCV5 <41875870+CarlosCV5@users.noreply.github.com>
2018-08-02 02:56:12 +0100

    Add files via upload
    Este é o primeiro depois de adicionar o login e o logout e com o registo na base de dados a funcionar .

--- fitapp/MainActivity.java
+++ fitapp/MainActivity.java
@@ -2,17 +2,33 @@ package com.example.valen.fitapp;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import static com.example.valen.fitapp.R.layout.activity_main;
 
 public class MainActivity extends AppCompatActivity {
     private Button loginbutton,registerbutton;
     ProgressBar progressbutton;
+    private EditText et_email, et_password;
+    private String email, password ;
+
+    private FirebaseAuth auth;
+
 
     @SuppressLint("WrongViewCast")
     @Override
@@ -24,6 +40,11 @@ public class MainActivity extends AppCompatActivity {
         registerbutton =  findViewById(R.id.register_button);
         progressbutton =  findViewById(R.id.login_progress);
         progressbutton.setVisibility(View.INVISIBLE);
+
+        et_email = (EditText) findViewById(R.id.email_space);
+        et_password = (EditText) findViewById(R.id.password_space);
+
+        auth = FirebaseAuth.getInstance();
 
         registerbutton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,17 +58,72 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 progressbutton.setVisibility(View.VISIBLE);
                 loginbutton.setVisibility(View.INVISIBLE);
+                Login();
             }
         });
 
     }
 
-    public void openactivity_registo(){
+    public void openactivity_registo()
+    {
         Intent intent = new Intent(this, Registo.class);
         startActivity(intent);
 
     }
+//////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
 
+//Funções que Determinam o Login
+
+    public void Login(){
+        intialize();
+        if (!validate()){
+            Toast.makeText(this, "Erro no Login", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            faz_login();
+        }
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        if (email.isEmpty()||!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            et_email.setError("Email Inválido!");
+            valid = false;
+        }
+        if (password.isEmpty()) {
+            et_password.setError("Coloque uma Password!");
+            valid = false;
+        }
+        return valid;
+    }
+
+    public void intialize()
+    {
+        email = et_email.getText().toString().trim();
+        password = et_password.getText().toString().trim();
+    }
+
+    public void faz_login()
+    {
+        auth.signInWithEmailAndPassword(email ,password).addOnCompleteListener(new OnCompleteListener<AuthResult>()
+        {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task)
+            {
+                if (task.isSuccessful())
+                {
+                    Toast.makeText(getApplicationContext(), "Login executado com sucesso" , Toast.LENGTH_SHORT).show();
+                    finish();
+                    Intent i = new Intent(getApplicationContext() , ProfileActivity.class);
+                    startActivity(i);
+                }
+                else
+                    Toast.makeText(getApplicationContext(), "Não conseguiu fazer log in" , Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
 
 }
--- fitapp/Registo.java
+++ fitapp/Registo.java
@@ -1,5 +1,7 @@
 package com.example.valen.fitapp;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Patterns;
@@ -8,10 +10,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Registo extends AppCompatActivity {
     private EditText et_name, et_email, et_password, et_cpassword;
     private String name, email, password, cpassword;
     Button registobutton;
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +31,9 @@ public class Registo extends AppCompatActivity {
         et_password = (EditText) findViewById(R.id.password_space);
         et_cpassword = (EditText) findViewById(R.id.cpassword_space);
         registobutton = (Button) findViewById(R.id.registo_button);
+
+        auth = FirebaseAuth.getInstance();
+
         registobutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -35,13 +47,13 @@ public class Registo extends AppCompatActivity {
         if (!validate()){
             Toast.makeText(this, "ERRO NO REGISTO", Toast.LENGTH_SHORT).show();
         }
-       // else {
-       //     Registo_success();
-      //  }
+        else {
+             create_user();
+          }
     }
-   // public void Registo_success(){
-        //COLOCAR OS INPUTS PARA A FIREBASE
-  //  }
+    // public void Registo_success(){
+    //COLOCAR OS INPUTS PARA A FIREBASE
+    //  }
     public boolean validate() {
         boolean valid = true;
         if (name.isEmpty()|| name.length() > 32) {
@@ -61,7 +73,7 @@ public class Registo extends AppCompatActivity {
             valid = false;
         }
 
-    return valid;
+        return valid;
 
     }
     public void intialize(){
@@ -71,6 +83,32 @@ public class Registo extends AppCompatActivity {
         cpassword = et_cpassword.getText().toString().trim();
 
 
-        }
-        CONA();
+    }
+
+    public void create_user()
+    {
+        auth.createUserWithEmailAndPassword(email , password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task)
+            {
+
+                if(task.isSuccessful())
+                {
+                    Toast.makeText(getApplicationContext() , "Utilizador criado" , Toast.LENGTH_SHORT).show();
+                  finish();
+
+                    Intent i = new Intent(getApplicationContext() , ProfileActivity.class);
+                    startActivity(i);
+                }
+
+
+                else
+                {
+                    Toast.makeText(getApplicationContext() , "NÃO DEU CARALHO" , Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
 }
